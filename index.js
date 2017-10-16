8600bbe70ed83f5b3a5663d2b56ede7edf9ff9d9
Luca Capra <luca.capra@gmail.com>
2017-10-16 07:36:13 +0200

    WIP subscribe example

--- index.js
+++ index.js
@@ -8,42 +8,81 @@ const code = '0001'
 const raptor = new Raptor(config.raptor)
 
 const loadDevice = (code) => {
-    log.info("Search device with code %s", code)
-    return raptor.Inventory().search({
-        properties: {code}
-    })
+    log.info('Search device with code %s', code)
+    return raptor.Inventory()
+        .search({
+            properties: {code}
+        })
         .then((result) => {
             // found a device
             if (result.length) {
-                log.info("Found device %s", result[0].name)
+                log.info('Found device %s', result[0].name)
                 return Promise.resolve(result[0])
             }
+
             // create a new device
-            log.info("Creating a new example device")
+            log.info('Creating a new example device')
+
             const device = new Raptor.models.Device()
             device.name = 'Environment monitor'
             device.properties.code = code
-            device
-                .addStream('ambient', {
+            device.setStream({
+                'name': 'ambient',
+                'channels': {
                     'temperature': 'number',
                     'light': 'number',
-                })
-                .addStream('battery', {
+                }
+            })
+            device.setStream({
+                'name': 'battery',
+                'channels': {
                     'charge': 'number',
-                })
+                }
+            })
+
+            log.debug('Creating device: %j', device.toJSON())
+
+            return raptor.Inventory().create(device)
         })
-    return raptor.Inventory().create(device)
 }
 
-raptor.Auth().login()
-    .then((user) => {
-        log.debug('Logged in as %s (id=%s)', user.username, user.uuid)
-        return loadDevice(code)
+const subscribe = (device) => {
+    return raptor.Inventory()
+        .subscribe(device, (data) => {
+            log.info('Data received: %j', data)
+        })
+        .then(() => Promise.resolve(device))
+}
+
+const pushData = (device) => {
+    const record = device.getStream('ambient').createRecord({
+        temperature: Math.floor(Math.random()*10),
+        light: Math.floor(Math.random()*100)
     })
-    .then((device) => {
-        log.debug('Got device %s', device.id)
-        return Promise.resolve()
-    })
-    .catch((e) => {
-        log.error(e)
-    })
+    return raptor.Stream().push(record)
+}
+
+const main = () => {
+
+    log.level = config.logLevel
+
+    raptor.Auth().login()
+        .then((user) => {
+            log.debug('Logged in as %s (id=%s)', user.username, user.uuid)
+            return loadDevice(code)
+        })
+        .then((device) => {
+            log.debug('Got device `%s`, subscribing to events', device.id)
+            return subscribe(device)
+        })
+        .then((device) => {
+            log.debug('Pushing data to device `%s`', device.id)
+            return pushData(device)
+        })
+        .catch((e) => {
+            log.error('Error: %s', e.message)
+        })
+}
+
+
+main()
